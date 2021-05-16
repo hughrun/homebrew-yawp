6abc618a866f2a62c495c7f8b21610dcbfa05907
Hugh Rundle <hugh@hughrundle.net>
2021-05-16 15:04:37 +1000

    try depends_on instead of uses_from_macos

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -5,10 +5,9 @@ class Yawp < Formula
   sha256 "fc6cd8b655c4e9a17cba170c6e3b280972b77956d75d2539cbda38f1e57147e3"
   license "AGPL-3.0-or-later"
 
+  depends_on "libcrypto"
+  depends_on "libssl"
   depends_on "rust" => :build
-
-  uses_from_macos "libcrypto"
-  uses_from_macos "libssl"
 
   def install
     system "cargo", "install", *std_cargo_args
