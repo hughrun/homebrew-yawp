e777029e77db57d09bc7d5eb927a210401fc0a77
Hugh Rundle <hugh@hughrundle.net>
2021-05-16 13:54:57 +1000

    add test to formula

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -3,11 +3,15 @@ class Yawp < Formula
   homepage "https://github.com/hughrun/yawp"
   url "https://github.com/hughrun/yawp/archive/v0.1.1.tar.gz"
   sha256 "fc6cd8b655c4e9a17cba170c6e3b280972b77956d75d2539cbda38f1e57147e3"
-  license "AGPL-3.0"
+  license "AGPL-3.0-or-later"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "testing123", shell_output("./target/release/yawp testing123")
   end
 end
