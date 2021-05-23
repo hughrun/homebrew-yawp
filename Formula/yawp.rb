ce92b1b7604ae521622ffc67855455767a9b3354
Hugh Rundle <17669239+hughrun@users.noreply.github.com>
2021-05-23 05:23:25 +0000

    yawp: add 0.1.2 bottle.

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -5,6 +5,12 @@ class Yawp < Formula
   sha256 "606fb5e55e210f3640147c075f142c1dea48ffce753ee7e31ebb8182ca2bb07e"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/hughrun/homebrew-yawp/releases/download/yawp-0.1.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "8eece1e806e81af9459ce4c0ea90443badb1ff8d3ecce963d8e2d1a99923df43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b931fd6def7aeade38aacf916768fc2dc1023b61fb9289a389f3fe83baeb2e2c"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl"
 
