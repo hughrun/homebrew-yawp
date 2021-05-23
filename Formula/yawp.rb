a23444844d853a8dd64c8493b19687bfb84984a7
Hugh Rundle <hugh@hughrundle.net>
2021-05-23 15:04:57 +1000

    update to v0.1.2
    Closes #3.
    
    Signed-off-by: Hugh Rundle <17669239+hughrun@users.noreply.github.com>

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -1,16 +1,9 @@
 class Yawp < Formula
   desc "Command-line app for publishing social media posts"
   homepage "https://github.com/hughrun/yawp"
-  url "https://github.com/hughrun/yawp/archive/v0.1.1.tar.gz"
-  sha256 "fc6cd8b655c4e9a17cba170c6e3b280972b77956d75d2539cbda38f1e57147e3"
+  url "https://github.com/hughrun/yawp/archive/v0.1.2.tar.gz"
+  sha256 "606fb5e55e210f3640147c075f142c1dea48ffce753ee7e31ebb8182ca2bb07e"
   license "AGPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/hughrun/homebrew-yawp/releases/download/yawp-0.1.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, catalina:     "67bfc5b258f5311d01732454323fe1f6ebdd53e4b8077fb534327dd9449b9e0e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "547b281df16ca2ffbc946885a87d3065176f7907a22a4037861f8b1fa1aee1e9"
-  end
 
   depends_on "rust" => :build
   depends_on "openssl"
