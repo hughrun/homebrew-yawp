aa55103636d0b88308bbe2f7dd76787d37e100af
Hugh Rundle <hugh@hughrundle.net>
2022-08-07 19:19:13 +1000

    remove old bottle

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -3,15 +3,7 @@ class Yawp < Formula
   homepage "https://github.com/hughrun/yawp"
   url "https://github.com/hughrun/yawp/archive/v0.1.3.tar.gz"
   sha256 "ee60d175b6589c3fd58773efe39131a77cbae53e2c250a7c5a0daa254f992f1f"
-
   license "AGPL-3.0-or-later"
-
-  bottle do
-    root_url "https://github.com/hughrun/homebrew-yawp/releases/download/yawp-0.1.2"
-    sha256 cellar: :any_skip_relocation, catalina:     "8eece1e806e81af9459ce4c0ea90443badb1ff8d3ecce963d8e2d1a99923df43"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b931fd6def7aeade38aacf916768fc2dc1023b61fb9289a389f3fe83baeb2e2c"
-  end
-
   depends_on "rust" => :build
   depends_on "openssl"
 
