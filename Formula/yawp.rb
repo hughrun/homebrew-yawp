d53a27c0f229be182b2d5b7a225de86320ea3117
Hugh Rundle <hugh@hughrundle.net>
2022-08-07 19:12:32 +1000

    remove blank line at EOF

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -3,7 +3,7 @@ class Yawp < Formula
   homepage "https://github.com/hughrun/yawp"
   url "https://github.com/hughrun/yawp/archive/v0.1.3.tar.gz"
   sha256 "ee60d175b6589c3fd58773efe39131a77cbae53e2c250a7c5a0daa254f992f1f"
-  
+
   license "AGPL-3.0-or-later"
 
   bottle do
