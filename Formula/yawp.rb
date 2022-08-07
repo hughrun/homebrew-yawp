edb7d3a8381375f2cdc4da6b37e406e58bb8515f
Hugh Rundle <hugh@hughrundle.net>
2022-08-07 19:30:03 +1000

    fix shasum

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -2,7 +2,7 @@ class Yawp < Formula
   desc "Command-line app for publishing social media posts"
   homepage "https://github.com/hughrun/yawp"
   url "https://github.com/hughrun/yawp/archive/v0.1.3.tar.gz"
-  sha256 "ee60d175b6589c3fd58773efe39131a77cbae53e2c250a7c5a0daa254f992f1f"
+  sha256 "0525bdb46e2725beafa2e9b189d7f61be6b9b080b9323ba8356b038d917530c4"
   license "AGPL-3.0-or-later"
   depends_on "rust" => :build
   depends_on "openssl"
