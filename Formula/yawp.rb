a15d88c9bb4eb0bb52551e631fddb75aabd74a78
Hugh Rundle <hugh@hughrundle.net>
2022-08-07 17:59:38 +1000

    update shasum and version number for 0.1.3

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -1,8 +1,8 @@
 class Yawp < Formula
   desc "Command-line app for publishing social media posts"
   homepage "https://github.com/hughrun/yawp"
-  url "https://github.com/hughrun/yawp/archive/v0.1.2.tar.gz"
-  sha256 "606fb5e55e210f3640147c075f142c1dea48ffce753ee7e31ebb8182ca2bb07e"
+  url "https://github.com/hughrun/yawp/archive/v0.1.3.tar.gz"
+  sha256 "0525bdb46e2725beafa2e9b189d7f61be6b9b080b9323ba8356b038d917530c4"
   license "AGPL-3.0-or-later"
 
   bottle do
