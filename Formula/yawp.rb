845b00402d024505c74c9ff17b4a374ad59975d6
Hugh Rundle <hugh@hughrundle.net>
2022-08-08 20:14:29 +1000

    yawp 0.1.4
    * update to v0.1.4
    * fix missing newline
    
    Closes #7.
    
    Signed-off-by: Hugh Rundle <17669239+hughrun@users.noreply.github.com>

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -1,8 +1,8 @@
 class Yawp < Formula
   desc "Command-line app for publishing social media posts"
   homepage "https://github.com/hughrun/yawp"
-  url "https://github.com/hughrun/yawp/archive/v0.1.3.tar.gz"
-  sha256 "0525bdb46e2725beafa2e9b189d7f61be6b9b080b9323ba8356b038d917530c4"
+  url "https://github.com/hughrun/yawp/archive/v0.1.4.tar.gz"
+  sha256 "412bffa4d04c0a6ebf216ce2653802ae3e8bf38be41cf306d68ceff85f5889b5"
   license "AGPL-3.0-or-later"
   depends_on "rust" => :build
   depends_on "openssl"
