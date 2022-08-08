14248754434008d677fc986beafa729ae34e61fb
Hugh Rundle <17669239+hughrun@users.noreply.github.com>
2022-08-08 10:43:12 +0000

    yawp: add 0.1.4 bottle.

--- Formula/yawp.rb
+++ Formula/yawp.rb
@@ -4,6 +4,12 @@ class Yawp < Formula
   url "https://github.com/hughrun/yawp/archive/v0.1.4.tar.gz"
   sha256 "412bffa4d04c0a6ebf216ce2653802ae3e8bf38be41cf306d68ceff85f5889b5"
   license "AGPL-3.0-or-later"
+
+  bottle do
+    root_url "https://github.com/hughrun/homebrew-yawp/releases/download/yawp-0.1.4"
+    sha256 cellar: :any_skip_relocation, big_sur:      "34902832fe8327eaaa5042ca89ec04193a103f2fab4752a1fab6ab26f6037ead"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5910e86da8dbe6e3585fef1752b936a307d428e1502f85c41753c6095d91bf41"
+  end
   depends_on "rust" => :build
   depends_on "openssl"
 
